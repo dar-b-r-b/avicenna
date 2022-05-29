6b953acca1ddefbbbc59e3c73f574ad1aec535c6
Барабанов Иван (СимбирСофт) <39259020+IOneB@users.noreply.github.com>
2022-05-29 15:58:16 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ const server = http.createServer(app);
 const { API_PORT } = process.env;
 const port = process.env.API_PORT;
 
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+server.listen();
