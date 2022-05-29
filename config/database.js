73a341629ba8b4ad2c26117fb4ede39ac04c0c93
Барабанов Иван (СимбирСофт) <39259020+IOneB@users.noreply.github.com>
2022-05-29 15:50:21 +0400

    new parameters

--- config/database.js
+++ config/database.js
@@ -2,10 +2,13 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  database: "avicennaDb",
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
   host: process.env.DB_HOST,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = { pool };
